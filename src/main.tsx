2cda614400eee2d6ba4922877762f6622a1bf9a9
Artem <bakirovartem69@gmail.com>
2025-05-20 21:18:43 +0200

    init sdk

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SdkService } from './utils/sdkService.ts'
+console.log('init sdk')
+void SdkService.init()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
