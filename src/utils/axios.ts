d873f267fde5d71e299258bb87900d680ac3fd67
Artem <bakirovartem69@gmail.com>
2025-08-06 17:47:49 +0200

    changed the baseUrl to new backend

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "https://bastyon-mini-app-production.up.railway.app/api",
+  baseURL: "https://express-server-production-aeca.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
