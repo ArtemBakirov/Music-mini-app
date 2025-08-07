099ae1db615c01e2dd7869a291d052798860a731
Artem <bakirovartem69@gmail.com>
2025-08-07 19:43:09 +0200

    Testing iframe

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -82,63 +82,8 @@ export default function Music() {
         " bg-[#371A4D] h-screen p-4 pt-12 w-full flex flex-col gap-4 items-center text-white"
       }
     >
-      <h2>Search music</h2>
-      <div className={"w-[80%]"}>
-        <SearchInput onClick={handleSearch} query={query} setQuery={setQuery} />
-      </div>
-
-      <button onClick={handleSearch}>Search</button>
-      <div className={"flex flex-col gap-4 padding-2 overflow-y-scroll"}>
-        {viewMode === "search" && searchResults ? (
-          searchResults?.pages.map((page, i) =>
-            page.items.map((item: any, idx: any) => {
-              const isLastItem =
-                i === searchResults.pages.length - 1 &&
-                idx === page.items.length - 1;
-              return (
-                <div key={idx}>
-                  <DisplaySongCard
-                    songData={item}
-                    idx={idx}
-                    key={item.videoId || idx}
-                  />
-                  {isLastItem && (
-                    <div
-                      ref={observerRef}
-                      className="h-6 border-2 border-purple-700"
-                    />
-                  )}
-                </div>
-              );
-            }),
-          )
-        ) : (
-          <>
-            {playlistResults &&
-              playlistResults?.songs &&
-              playlistResults.songs.map((item: any, idx: any) => {
-                return (
-                  <>
-                    <DisplaySongCard
-                      songData={item}
-                      idx={idx}
-                      key={item.videoId || idx}
-                    />
-                    {/*isLastItem && (
-                  <div
-                    ref={observerRef}
-                    className="h-6 border-2 border-purple-700"
-                  />
-                )*/}
-                  </>
-                );
-              })}
-          </>
-        )}
-      </div>
-
+      DOES THIS WORK??
       {/* <div ref={observerRef} className="h-10" /> */}
-
       {isFetchingNextPage && <div>Loading more...</div>}
     </div>
   );
