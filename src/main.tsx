f23fcf31de9ac9cf36e5107d8fa4a41f2dfb2e9f
Artem <bakirovartem69@gmail.com>
2025-08-07 19:27:57 +0200

    Testing iframe

--- src/main.tsx
+++ src/main.tsx
@@ -31,7 +31,7 @@ createRoot(document.getElementById("root")!).render(
             element={<Layout />}
             children={
               <>
-                <Route index element={<TestIframe />} />
+                <Route index element={<Music />} />
                 <Route path="playlists" element={<PlayLists />} />
                 <Route
                   path="playlists/:playlistId"
