51943583edabf1db80748da6320cacc84956efe1
Artem <bakirovartem69@gmail.com>
2025-09-06 22:35:10 +0200

    try to play and pause again

--- src/components/DisplayYoutubeSongCard.tsx
+++ src/components/DisplayYoutubeSongCard.tsx
@@ -88,7 +88,7 @@ export const DisplayYoutubeSongCard = forwardRef<YtTrackHandle, Props>(
       p.unMute();
       p.setVolume(70);
       console.log("pause, after play");
-      // await new Promise((r) => setTimeout(r, 1000));
+      await new Promise((r) => setTimeout(r, 1000));
       p.pauseVideo();
       // Leave muted/paused. Real user plays will unmute in `play()`.
     };
