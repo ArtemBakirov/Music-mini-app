cf3b99db1bf2343879e68c0dda0df10869f53cd0
Artem <bakirovartem69@gmail.com>
2025-10-09 23:47:39 +0200

    mobile view for tracks search

--- src/utils/MusicPlayerManager.ts
+++ src/utils/MusicPlayerManager.ts
@@ -314,7 +314,7 @@ export class MusicPlayerManager {
     if (e.data === YT.PlayerState.PAUSED || e.data === YT.PlayerState.ENDED) {
       console.log("state changed to Ended", YT.PlayerState);
       console.log("on ended, YT.Player", YT.Player);
-      console.log("on ended, play again", YT.Player.playVideo());
+      YT.Player.playVideo();
     }
 
     // setIsPlaying(false);
